e3cf64d84221df0aff7dc0e4c613bd7be08a7e97
rich1e <v.richkov@gmail.com>
2021-08-12 15:51:38 +0300

    JRTB-3: added Command pattern for handling Telegram Bot commands, updated RELEASE_NOTES.md

--- src/main/java/com/github/javarushcommunity/jrtb/bot/JavaRushTelegramBot.java
+++ src/main/java/com/github/javarushcommunity/jrtb/bot/JavaRushTelegramBot.java
@@ -1,17 +1,21 @@
 package com.github.javarushcommunity.jrtb.bot;
 
+import com.github.javarushcommunity.jrtb.command.CommandContainer;
+import com.github.javarushcommunity.jrtb.service.SendBotMessageServiceImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import static com.github.javarushcommunity.jrtb.command.CommandName.NO;
 
 /**
- * Telegrambot for Javarush Community from Javarush community.
+ * Telegram bot for Javarush Community from Javarush community.
  */
 @Component
-class JavarushTelegramBot extends TelegramLongPollingBot {
+public class JavaRushTelegramBot extends TelegramLongPollingBot {
+
+    public static String COMMAND_PREFIX = "/";
 
     @Value("${bot.username}")
     private String username;
@@ -19,21 +23,22 @@ class JavarushTelegramBot extends TelegramLongPollingBot {
     @Value("${bot.token}")
     private String token;
 
+    private final CommandContainer commandContainer;
+
+    public JavaRushTelegramBot() {
+        this.commandContainer = new CommandContainer(new SendBotMessageServiceImpl(this));
+    }
+
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
-            String massage = update.getMessage().getText().trim();
-            String chatId = update.getMessage().getChatId().toString();
+            String message = update.getMessage().getText().trim();
+            if (message.startsWith(COMMAND_PREFIX)) {
+                String commandIdentifier = message.split(" ")[0].toLowerCase();
 
-            SendMessage sm = new SendMessage();
-            sm.setChatId(chatId);
-            sm.setText(massage);
-
-            try {
-                execute(sm);
-            } catch (TelegramApiException e) {
-                // todo add logging to the project
-                e.printStackTrace();
+                commandContainer.retrieveCommand(commandIdentifier).execute(update);
+            } else {
+                commandContainer.retrieveCommand(NO.getCommandName()).execute(update);
             }
         }
     }

96d80b618d948ec1428f59950225d3d13c7ca978
rich1e <v.richkov@gmail.com>
2021-08-19 18:45:11 +0300

    STEP_7 JRTB-7: added the ability to delete group subscription.

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -19,5 +19,9 @@
 
 ## 0.5.0-SNAPSHOT
 
-*   JRTB-5: added ability to subscribe on group
-*   JRTB-6: added ability to get a list of group subscriptions.
+* JRTB-5: added ability to subscribe on group
+* JRTB-6: added ability to get a list of group subscriptions.
+
+## 0.6.0-SNAPSHOT
+
+*   JRTB-7: added the ability to delete group subscription.
--- src/main/java/com/github/javarushcommunity/jrtb/JavarushTelegramBotApplication.java
+++ src/main/java/com/github/javarushcommunity/jrtb/JavarushTelegramBotApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavarushTelegramBotApplication {
+public class   JavarushTelegramBotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavarushTelegramBotApplication.class, args);

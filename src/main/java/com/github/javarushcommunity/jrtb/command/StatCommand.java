c8e532cc41d28dc1cd9dd854c42c1351a04f0c55
rich1eo <72224397+rich1eo@users.noreply.github.com>
2021-08-20 15:21:09 +0300

    STEP_8_JRTB-4,8,9:  added ability to send notifications about new articles,added ability to set inactive telegram user, added ability to set active user and/or start using it. (#17)

--- src/main/java/com/github/javarushcommunity/jrtb/command/StatCommand.java
+++ src/main/java/com/github/javarushcommunity/jrtb/command/StatCommand.java
@@ -19,7 +19,7 @@ public class StatCommand implements Command {
 
     @Override
     public void execute(Update update) {
-        int activeUserCount = telegramUserService.retrieveAllActiveUsers().size();
+        int activeUserCount = telegramUserService.findAllActiveUsers().size();
         sendBotMessageService.sendMessage(update.getMessage().getChatId().toString(), String.format(STAT_MESSAGE, activeUserCount));
     }
 }
---
+++ src/main/java/com/github/javarushcommunity/jrtb/javarushclient/dto/LikeStatus.java
@@ -0,0 +1,30 @@
+package com.github.javarushcommunity.jrtb.javarushclient.dto;
+
+public enum LikeStatus {
+    UNKNOWN,
+    LIKE,
+    HOT,
+    FOLLOW,
+    FAVORITE,
+    SOLUTION,
+    HELPFUL,
+    ARTICLE,
+    OSCAR,
+    DISLIKE,
+    WRONG,
+    SPAM,
+    ABUSE,
+    FOUL,
+    TROLLING,
+    OFFTOPIC,
+    DUPLICATE,
+    DIRTY,
+    OUTDATED,
+    BORING,
+    UNCLEAR,
+    HARD,
+    EASY,
+    FAKE,
+    SHAM,
+    AWFUL
+}
